04c9fa8e07247a15ba57009c1225e7f356e13f14
agung <joyoagung170@gmail.com>
2020-05-28 19:02:46 +0700

    Edit Profile

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,9 +1,9 @@
 <footer class="main-footer">
   <div class="container my-auto">
-    <strong>Copyright &copy; SIPEKAteam <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong> &copy; SIPEKAteam <a href="http://adminlte.io"></a></strong>
+    
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.3-pre <?= date('Y'); ?>
+      <b>Version</b>  <?= date('Y'); ?>
     </div>
   </div>
 </footer>
