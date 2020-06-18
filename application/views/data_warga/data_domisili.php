d943778fd9c45901449a4a5120db8ca8ab586c01
yudi iriyanto <52985439+riyan-web@users.noreply.github.com>
2020-06-18 21:27:40 +0700

    perbaikan menu dashboard admin

--- application/views/data_warga/data_domisili.php
+++ application/views/data_warga/data_domisili.php
@@ -5,12 +5,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Data Warga</h1>
+                    <h1>Data Warga Domisili</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Data Warga</li>
+                        <li class="breadcrumb-item"><a href="#">Domisili</a></li>
+                        <li class="breadcrumb-item active">Data Domisili</li>
                     </ol>
                 </div>
             </div>
@@ -24,7 +24,7 @@
 
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Kartu Keluarga</h3>
+                        <h3 class="card-title">Warga Berdomisili Luar</h3>
                     </div>
                     <!-- /.card-header -->
                     <?= $this->session->flashdata('message'); ?>
